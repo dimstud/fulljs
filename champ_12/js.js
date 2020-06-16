f6aa5c58e516f68537b0e68222b97d4e5a646bf9
Dmitry Ageev <dimichageev@icloud.com>
2020-06-16 10:38:59 +0300

    Начал урок 12

---
+++ champ_12/js.js
@@ -0,0 +1,14 @@
+let options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
+
+console.log(options.name);
+options.bool = false;
+options.color = {
+    border: "black",
+    bg: "red"
+};
+
+console.log(options);
