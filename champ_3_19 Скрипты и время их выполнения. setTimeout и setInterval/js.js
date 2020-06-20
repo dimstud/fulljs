06477e122a719372c01a3dc51a8f6ab7d0b3a85b
Dmitry Ageev <dimichageev@icloud.com>
2020-06-20 14:15:33 +0300

    Остановился на 9:00 сделал разметку

--- champ_3_19 Скрипты и время их выполнения. setTimeout и setInterval/js.js
+++ champ_3_19 Скрипты и время их выполнения. setTimeout и setInterval/js.js
@@ -1,9 +1,14 @@
 // let timerId = setTimeout(sayHello, 3000);
 // clearTimeout(timerId);
 
-let timerId = setInterval(sayHello, 3000);
-clearTimeout(timerId);
+// let timerId = setInterval(sayHello, 3000);
+// clearTimeout(timerId);
 
-function sayHello() {
-    console.log('Hello World');
-}
+// function sayHello() {
+//     console.log('Hello World');
+// }
+
+// let timerId = setTimeout(function log() {
+//     console.log("Hello");
+//     setTimeout(log, 2000);
+// });
