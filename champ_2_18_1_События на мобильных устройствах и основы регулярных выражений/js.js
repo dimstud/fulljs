2d36c6c11136e93fc8cd1fdbc7dbb1119f22bbbd
Dmitry Ageev <dimichageev@icloud.com>
2020-06-20 10:23:04 +0300

    Выполнил chamo_18_1

--- champ_2_18_1_События на мобильных устройствах и основы регулярных выражений/js.js
+++ champ_2_18_1_События на мобильных устройствах и основы регулярных выражений/js.js
@@ -17,7 +17,7 @@
 //Когда точка соприкоснавения уходит за область браузера
 //touchcencel
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     let box = document.querySelector('.box');
 
     // box.addEventListener('touchstart', function(e) {
@@ -37,13 +37,75 @@ window.addEventListener('DOMContentLoaded', function() {
     //     console.log(e.targetTouches);
     // });
 
-    box.addEventListener('touchmove', function(e) {
+    box.addEventListener('touchmove', function (e) {
         e.preventDefault();
         console.log("Red box: " + e.touches[0].pageX);
     });
 
-//     box.addEventListener('touchend', function(e) {
-//         e.preventDefault();
-//         console.log("Red box: touchend");
-//     });
+    //     box.addEventListener('touchend', function(e) {
+    //         e.preventDefault();
+    //         console.log("Red box: touchend");
+    //     });
+
+
+
+
+    //Регулярные выражения
+
+
+    // 1 вариант записи
+    // new RegExp('pattern', 'flags');
+
+    // 2 вариант записи
+    // /pattern/flags
+
+    // let ans = prompt('Введите ваше имя');
+
+    // let reg = /n/gi;
+    // // Ищем в строке букву n
+    // // console.log(ans.search(reg));
+
+    // // Работает с флагом g
+    // // console.log(ans.match(reg));
+    //Булево
+    // console.log(reg.test(ans));
+
+
+    //Флаги
+
+    // i - изкать независемо от регистра
+    // g - глобально во всех вхождениях все
+    // m - многострочность
+    // . - ищет или заменять любые символы
+
+
+    //Классы символов
+    // \d - найти цифру
+    // \D - найти не цифру
+    // \w - найти буквы
+    // \W - найти не букву
+    // \s - найти пробелы
+    // \S - найти не пробелы
+
+
+
+    //Метод Реплейс
+    // let pass = prompt("Введите пароль");
+
+    // //Скрывает заменяя на *
+    // console.log(pass.replace(/./g, "*"));
+    // // Заменяеь - на :
+    // alert('12-34-66'.replace(/-/g, ":"));
+
+    //Ищем все цифры
+    // let ans = prompt('Введите число');
+
+    // let reg = /\d/g;
+
+    // // Работает с флагом g
+    // console.log(ans.match(reg));
+
+    let str = 'My name is R2D2';
+
+    console.log(str.match(/\w\d\w\d/i));
 });
