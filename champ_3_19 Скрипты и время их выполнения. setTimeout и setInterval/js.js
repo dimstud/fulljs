97883420d2eaeb9c4f1685526e9a45721337d767
Dmitry Ageev <dimichageev@icloud.com>
2020-06-25 12:45:56 +0300

    Выполнил champ_3_19

--- champ_3_19 Скрипты и время их выполнения. setTimeout и setInterval/js.js
+++ champ_3_19 Скрипты и время их выполнения. setTimeout и setInterval/js.js
@@ -12,3 +12,35 @@
 //     console.log("Hello");
 //     setTimeout(log, 2000);
 // });
+
+let btn = document.querySelector('.btn'),
+    elem = document.querySelector('.box');
+
+function myAnimation() {
+    let pos = 0;
+
+    let id = setInterval(frame, 10);
+    function frame () {
+        if (pos == 300) {
+            clearInterval();
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+let btnBlock = document.querySelector('.btn-block'),
+    btns = document.getElementsByTagName('button');
+
+btnBlock.addEventListener('click', function(e) {
+    // if (e.target && e.target.classList.contains('first')) {
+    //     console.log("Hello");
+    // }
+    if (e.target && e.target.classList.matches('button.first')) {
+        console.log("Hello");
+    }
+});
