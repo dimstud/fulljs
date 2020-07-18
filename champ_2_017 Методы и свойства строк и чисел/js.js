38fc5fe66ac84bdb4dff587aa41d943d991c38f3
Dmitry Ageev <dimichageev@icloud.com>
2020-07-18 15:27:44 +0300

    champ_2_017 оставновился на 10:00

---
+++ champ_2_017 Методы и свойства строк и чисел/js.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 4];
+
+// console.log(arr.length); // Количество элементов
+console.log(str.toUpperCase()); // Заглавные буквы
+console.log(str.toLowerCase()); // Строчные буквы
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf("fruit")); // количество символов перед словом
