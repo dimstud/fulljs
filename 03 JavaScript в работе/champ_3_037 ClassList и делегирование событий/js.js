ccd7da00c0b18b6941ab3357b29ffd6d4d50ded8
Dmitry Ageev <dimichageev@icloud.com>
2020-08-03 15:31:12 +0300

    champ_3_037 закончил

--- 03 JavaScript в работе/champ_3_037 ClassList и делегирование событий/js.js
+++ 03 JavaScript в работе/champ_3_037 ClassList и делегирование событий/js.js
@@ -1,19 +1,60 @@
-"use strict";
-// defer - 1) загружается в фоновом режиме и подключается когда всё загрузится 2) скрипты ждут друг друга
-let p = document.querySelectorAll('p');
-console.log(p);
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-// async - 1) страница нне ждет загрузку скриптов 2)скрипты не ждут друг друга
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0)); // получает класс под определенным индексом
+// console.log(btns[1].classList.add('red', 'fash')); // добавляет класс
+// console.log(btns[0].classList.remove('blue')); // удаляет класс
+// console.log(btns[0].classList.toggle('blue')); // услиесть класс, то уберётся, а если нет класса, то добавиться
 
-//  Помещение  тэга  script в DOM
+// contains для условие 
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
 
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+// Для меню-гамбургер
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
 
-loadScript("test.js");
-loadScript("same.js");              
+
+    //  невсегда
+    btns[1].classList.toggle('red');
+});
+
+//  older
+console.log(btns[0].className);
+
+// Делегирование событий
+wrapper.addEventListener('click', (event) => {
+    // console.dir(event.target);
+
+    //  лучше
+    // if (event.target && event.target.tagName == "BUTTON") {
+    //     console.log('Hello!');
+    // }
+
+    // Обычный
+    // if ( event.target && event.target.classList.contains('blue') ) {
+    //     console.log('Hello!');
+    // }
+
+    // от Google matches-совпадает с чем то
+    if (event.target && event.target.matches("button.red")) {
+        console.log('Hello!');
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello!');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
