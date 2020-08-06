572fbe4ea5f987ffdfe81ebb5443064e109c93bd
Dmitry Ageev <dimichageev@icloud.com>
2020-08-06 23:31:51 +0300

    champ_3_046 закончил

--- 03 JavaScript в работе/champ_3_046 Контекст вызова. This/script.js
+++ 03 JavaScript в работе/champ_3_046 Контекст вызова. This/script.js
@@ -1,2 +1,107 @@
 'use strict';
 
+// function showThis(a, b) {
+//     console.log(this);
+
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+
+// // const obj = {
+// //     a: 20,
+// //     b: 15,
+// //     sum: function() {
+// //         // console.log(this);
+// //         function shout() {
+// //             console.log(this);
+// //         }
+// //         shout();
+// //     }
+// // };
+
+// // obj.sum();
+
+
+
+// // function User(name, id) {
+// //     this.name = name;
+// //     this.id = id;
+// //     this.human = true;
+// //     this.hello = function() {
+// //         console.log(`Hello ${this.name}`);
+// //     }
+// // }
+// // let ivan = new User('Ivan', 23);
+
+
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// //  Привязывает контекст выхова
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// //  Создаёт новую функцию
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log( double(3) );
+// console.log( double(13) );
+
+
+
+//  1) Обычная функция this = window,  о если use strict - undefined
+//  2) Контекст у методов объекта - сам объект
+//  3) this в конструкторах и классах - это новый экземпляр объектов
+//  4) Ручная привязка this: call, apply, bind
+
+
+
+
+
+const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+//     this.style.backgroundColor = 'red';
+// });
+
+// this не будет работать
+btn.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'red';
+});
+
+//  Будет брать выше и станет объектом
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+// const double = (a) => {
+//     return a * 2;
+// }
+// Тоже самое что и выше
+const double = a => a * 2;
+console.log( double(4) );
