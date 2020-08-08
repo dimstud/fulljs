829415d9a4d7e34e75b2ff57774e8a9d91fc9e6b
Dmitry Ageev <dimichageev@icloud.com>
2020-08-08 14:10:34 +0300

    champ_4_051 закончил

--- 04 Продвинутый JavaScript/champ_3_051 JSON формат передачи данных, глубокое клонирование объектов/script.js
+++ 04 Продвинутый JavaScript/champ_3_051 JSON формат передачи данных, глубокое клонирование объектов/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const persone = {
+    name: 'Alex',
+    tel: '+74444444444',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mikle'
+    }
+};
+
+// Для передачи данных на сервер stringify, принимает данные от сервера persone
+// console.log( JSON.parse(JSON.stringify(persone)) );
+
+//  Превратит в глубокий клон
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
