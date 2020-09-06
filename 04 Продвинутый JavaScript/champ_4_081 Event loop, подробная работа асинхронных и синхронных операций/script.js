30220210f5f05a3546b6f66c692099208465fe9c
Dmitry Ageev <dimichageev@icloud.com>
2020-09-06 15:15:12 +0300

    champ_4_081 готов

---
+++ 04 Продвинутый JavaScript/champ_4_081 Event loop, подробная работа асинхронных и синхронных операций/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('timeout_4000');
+// }, 4000);
+
+// console.log(2);
+
+
+
+
+
+
+
+
+// console.log(1);
+
+
+// сработает первым
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 4000);
+
+// setTimeout(() => {
+//     console.log('timeout_4000');
+// }, 4000);
+
+// console.log(2);
+
+
+// загружаем браузер
+
+// let k = 0;
+
+// 1e9 - единица с девятью нулями
+// function count() {
+//     for (let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+//     alert('done');
+// }
+
+// count();
+
+
+
+
+setTimeout(() => {
+    console.log(1);
+}, 0);
+
+console.log(2);
