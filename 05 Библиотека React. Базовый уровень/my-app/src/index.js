e76c3d53042511a72d81be3f2050ee38a083de79
Dmitry Ageev <dimichageev@gmail.com>
2020-11-18 23:21:59 +0300

    091 8m

--- 05 Библиотека React. Базовый уровень/my-app/src/index.js
+++ 05 Библиотека React. Базовый уровень/my-app/src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './components/app';
 import AppHeader from './components/app-header';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+function WhoAmI({name, surname, link}) {
+    return (
+        <>
+            <h1>My name is {name}, surname - {surname}</h1>
+            <a href={link}> My profile</a>
+        </>
+    )
+}
+
+const All = () => {
+	return (
+		<>
+			<WhoAmI name="Jhon" surname="Smith" link="facebook.com" />
+			<WhoAmI name="Ivan" surname="Smith" link="vk.com" />
+			<WhoAmI name="Alex" surname="Smith" link="facebook.com" />
+		</>
+	)
+}
+
+ReactDOM.render(<All/>, document.getElementById('root'));
 
