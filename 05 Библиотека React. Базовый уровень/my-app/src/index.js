45ed3a7bbcffff7705da36e9ad70e3cd3e73240b
Dmitry Ageev <dimichageev@gmail.com>
2020-11-19 16:02:07 +0300

    091 end

--- 05 Библиотека React. Базовый уровень/my-app/src/index.js
+++ 05 Библиотека React. Базовый уровень/my-app/src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './components/app';
 import AppHeader from './components/app-header';
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 26
+		}
+		this.nextYear = this.nextYear.bind(this);
+		// this.nextYear = () => {
+		// 	this.setState(state => ({
+		// 		years: ++state.years
+		// 	}))
+		// }
+	// }
+	// state = {
+	// 	years: 26
+	}
 
-function WhoAmI({name, surname, link}) {
-    return (
-        <>
-            <h1>My name is {name}, surname - {surname}</h1>
-            <a href={link}> My profile</a>
-        </>
-    )
+	nextYear = () => {
+		this.setState(state => ({
+			years: ++state.years
+		}))
+	}
+
+	// nextYear() {
+	// 	this.setState(state => ({
+	// 		years: ++state.years
+	// 	}))
+	// }
+	render() {
+		const {name, surname, link} = this.props;
+		const {years} = this.state;
+		return (
+			<>
+				<button onClick={this.nextYear}>++</button>
+				<h1>My name is {name}, surname - {surname}, years = {years}</h1>
+				<a href={link}> My profile</a>
+			</>
+		 )
+	}
 }
+
 
 const All = () => {
 	return (
