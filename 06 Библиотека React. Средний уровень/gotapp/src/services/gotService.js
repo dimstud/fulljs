557a9063e751b28423a9730a53eb020f7ecafb14
Dmitry Ageev <dimichageev@gmail.com>
2020-11-23 17:21:36 +0300

    097 16m

--- 06 Библиотека React. Средний уровень/gotapp/src/services/gotService.js
+++ 06 Библиотека React. Средний уровень/gotapp/src/services/gotService.js
@@ -49,4 +49,23 @@ export default class GotService {
             culture: char.culture
         }
     }
+
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publiser: book.publiser,
+            released: book.released
+        }
+    }
 }
