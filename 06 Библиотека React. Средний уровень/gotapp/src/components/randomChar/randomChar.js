f7a79265826e22c7a7de18c32cf4ae1a288d36f4
Dmitry Ageev <dimichageev@gmail.com>
2020-11-25 17:27:06 +0300

    098 end

--- 06 Библиотека React. Средний уровень/gotapp/src/components/randomChar/randomChar.js
+++ 06 Библиотека React. Средний уровень/gotapp/src/components/randomChar/randomChar.js
@@ -32,8 +32,7 @@ export default class RandomChar extends Component {
     }
 
     updateChar() {
-        // const id = Math.floor(Math.random()*140 + 25);
-        const id = 1000000;
+        const id = Math.floor(Math.random()*140 + 25);
         this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
